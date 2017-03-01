443cc201d690bfd90d32c5bda2057adf887b1623
nitaybz <nitaybz@gmail.com>
2017-03-01 08:38:28 +0200

    Restore Old Plugin

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
-# homebridge-http-multiswitch
+# homebridge-http-homebridge
+Simple HTTP switches for Homebridge - stateful and radio-button/multi-switch switches
+
+*Forked from homebridge-switcheroo
+
+## Switch Services
+
+### Switch (standard on/off)
+Meant to be used as a standard on/off switch. Light, projector, fan, etc.
+
+```
+{
+        "accessory": "HttpMultiswitch",
+        "switch_type": "Switch",
+        "name": "My Projector",
+        "http_method": "GET",
+        "base_url": "http://192.168.0.XXX/command",
+        "on_url": "/on",
+        "off_url": "/off"
+}
+```
+
+### Multiswitch (radio buttons)
+Meant to be used as a switcher, where only one input is ever on.
+Automaticaly turns off the other switches when turning on one ofe them.
+
+Multiswitch appends the string, defined under 'multiurls' below, to complete the path of the Url (Must be use in the same order of the devices in "multiswitch".
+For example, when `PC Mode` is selected, the url generated will be `http://192.168.0.XXX/command?id=42786sdf787`. 
+```
+{
+    "accessory": "HttpMultiswitch",
+    "switch_type": "Multiswitch",
+    "name": "My Multiswitch",
+    "http_method": "GET",
+    "base_url": "http://192.168.0.XXX/command?id=",
+    "multiswitch": [
+                "Apple TV Mode",
+                "PC Mode",
+                "Android Mode",
+                "The Music Mode"
+            ],
+    "multiurls": [
+                "43",
+                "42786sdf787",
+                "l1479461871215",
+                "44"
+            ]
+}
+```
+
+## Configuration Params
+
+|             Parameter            |                       Description                       | Required |
+| -------------------------------- | ------------------------------------------------------- |:--------:|
+| `name`                           | name of the accessory                                   |          |
+| `switch_type`                    | `Switch` or `Multiswitch`                               |     ✓    |
+| `base_url`                       | url endpoint for whatever is receiving these requests   |     ✓    |
+| `http_method`                    | `GET` (default), `POST`,  `PUT`, `DELETE`               |          |
+| `username`                       | username for request                                    |          |
+| `password`                       | password for request                                    |          |
+| `send_immediately`               | option for request                                      |          |
+| `on_url` (only Switch)           | endpoint for the on state                               |     ✓    |
+| `off_url` (only Switch)          | endpoint for the off state                              |     ✓    |
+| `on_body` (only Switch)          | body for on state request                               |          |
+| `off_body` (only Switch)         | body for off state request                              |          |
+| `multiswitch` (only Multiswitch) | list of inputs for the Multiswitch - order is respected |     ✓    |
+| `multiurls` (only Multiswitch)   | list of endpoint urls for multiswitch-order is respected|     ✓    |
+
+## Help
+
+  - Make sure specify a port in the if necessary. (i.e. `"base_url" : "http://192.168.0.XXX:2000"`)
+  - Verify the correct `http_method` is begin used. Switcheroo defaults to `GET`
+
+## Installation
+Read about an example Raspberry Pi + Homebridge setup guide with this package [here](https://github.com/chriszelazo/Apartment-Homebridge-Setup).
+
+1. Install homebridge using: `npm install -g homebridge`
+2. Install homebridge-http using: `npm install -g homebridge-http-multiswitch`
+3. Update your config file
---
+++ index.js
@@ -0,0 +1,189 @@
+'use strict';
+
+var Service;
+var Characteristic;
+var request = require('request');
+
+module.exports = function (homebridge) {
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    homebridge.registerAccessory('homebridge-http-multiswitch', 'HttpMultiswitch', HttpMultiswitch);
+};
+
+function HttpMultiswitch(log, config) {
+    this.log = log;
+
+    this.name            = config.name             || 'MultiSwitch';
+    this.switchType      = config.switch_type;           
+    this.baseUrl         = config.base_url;
+    this.httpMethod      = config.http_method      || 'GET';
+
+    this.username        = config.username         || '';
+    this.password        = config.password         || '';
+    this.sendImmediately = config.send_immediately || '';
+
+    switch (this.switchType) {
+        case 'Switch':
+            this.onUrl   = this.baseUrl + config.on_url;
+            this.offUrl  = this.baseUrl + config.off_url;
+            this.onBody  = config.on_body  || '';
+            this.offBody = config.off_body || '';
+            break;
+
+        case 'Multiswitch':
+            this.multiswitch = config.multiswitch;
+            this.multiurls = config.multiurls;
+            break;
+
+        default:
+            throw new Error('Unknown homebridge-http-multiswitch switch type');
+    }
+}
+
+HttpMultiswitch.prototype = {
+
+    httpRequest: function(url, body, method, username, password, sendimmediately, callback) {
+        request({
+            url: url,
+            body: body,
+            method: method,
+            rejectUnauthorized: false,
+            auth: {
+                user: username,
+                pass: password,
+                sendImmediately: sendimmediately
+            }
+        },
+        function(error, response, body) {
+            callback(error, response, body);
+        });
+    },
+
+    setPowerState: function(targetService, powerState, callback, context) {
+        var funcContext = 'fromSetPowerState';
+        var reqUrl = '', reqBody = '';
+
+        // Callback safety
+        if (context == funcContext) {
+            if (callback) {
+                callback();
+            }
+
+            return;
+        }
+
+        switch(this.switchType) {
+            case 'Switch':
+                if (!this.onUrl || !this.offUrl) {
+                    this.log.warn('Ignoring request; No power state urls defined.');
+                    callback(new Error('No power state urls defined.'));
+                    return;
+                }
+
+                reqUrl  = powerState ? this.onUrl  : this.offUrl;
+                reqBody = powerState ? this.onBody : this.offBody;
+                break;
+
+            case 'Multiswitch':
+                this.services.forEach(function (switchService, idx) {
+                    if (idx === 0) {
+                        // Don't check the informationService which is at idx=0
+                        return;
+                    }
+
+                    if (targetService.subtype === switchService.subtype) {
+                        reqUrl = this.baseUrl + this.multiurls[idx-1];
+                    } else {
+                        switchService.getCharacteristic(Characteristic.On).setValue(false, undefined, funcContext);
+                    }
+                }.bind(this));
+                break;
+
+            default:
+                this.log('Unknown homebridge-http-multiswitch type in setPowerState');
+        }
+
+        this.httpRequest(reqUrl, reqBody, this.httpMethod, this.username, this.password, this.sendImmediately, function(error, response, responseBody) {
+            if (error) {
+                this.log.error('setPowerState failed: ' + error.message);
+                this.log('response: ' + response + '\nbody: ' + responseBody);
+            
+                callback(error);
+            } else {
+                switch (this.switchType) {
+                    case 'Switch':
+                        this.log.info('==> ' + (powerState ? "On" : "Off"));
+                        break;
+                    case 'Multiswitch':
+                        this.log('==> ' + targetService.subtype);
+                        break;
+                    default:
+                        this.log.error('Unknown switchType in request callback');
+                }
+
+                callback();
+            }
+        }.bind(this));
+    },
+
+    identify: function (callback) {
+        this.log('Identify me Senpai!');
+        callback();
+    },
+
+    getServices: function () {
+        this.services = [];
+
+        var informationService = new Service.AccessoryInformation();
+        informationService
+            .setCharacteristic(Characteristic.Manufacturer, 'Http-MultiSwitch')
+            .setCharacteristic(Characteristic.Model, 'Http-MultiSwitch');
+        this.services.push(informationService);
+
+        switch (this.switchType) {
+            case 'Switch':
+                this.log('(switch)');
+
+                var switchService = new Service.Switch(this.name);
+                switchService
+                    .getCharacteristic(Characteristic.On)
+                    .on('set', this.setPowerState.bind(this, switchService));
+
+                this.services.push(switchService);
+
+                break;
+            case 'Multiswitch':
+                this.log('(multiswitch)');
+
+                for (var i = 0; i < this.multiswitch.length; i++) {
+                    var switchName = this.multiswitch[i];
+
+                    switch(i) {
+                        case 0:
+                            this.log.warn('---+--- ' + switchName); break;
+                        case this.multiswitch.length-1:
+                            this.log.warn('   +--- ' + switchName); break;
+                        default:
+                            this.log.warn('   |--- ' + switchName);
+                    }
+
+                    var switchService = new Service.Switch(switchName, switchName);
+
+                    // Bind a copy of the setPowerState function that sets 'this' to the accessory and the first parameter
+                    // to the particular service that it is being called for. 
+                    var boundSetPowerState = this.setPowerState.bind(this, switchService);
+                    switchService
+                        .getCharacteristic(Characteristic.On)
+                        .on('set', boundSetPowerState);
+
+                    this.services.push(switchService);
+                }
+
+                break;
+            default:
+                this.log('Unknown homebridge-http-multiswitch type in getServices');
+        }
+        
+        return this.services;
+    }
+};
