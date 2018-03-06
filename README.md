781b96a596ad4ebc864bcd03cdd69b3d25e215f7
Jonathan Bell <jon@jonbell.net>
2018-03-05 23:16:22 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,88 +1,17 @@
-# homebridge-http-homebridge
-Simple HTTP switches for Homebridge - stateful and radio-button/multi-switch switches
+# homebridge-synology-surveillance-homemode
 
-*Forked from homebridge-switcheroo
+Simple Homebridge plugin to turn on/off homemode on Synology Surveillance station.
 
-_________________________________________
-#### Creating and maintaining Homebridge plugins consume a lot of time and effort, if you would like to share your appreciation, feel free to "Star" or donate. 
+Switch "on" -> home mode off (cameras are on)
+Switch "off" -> home mode on (cameras are off)
 
-<a target="blank" href="https://www.paypal.me/nitaybz"><img src="https://img.shields.io/badge/Donate-PayPal-blue.svg"/></a>
-<a target="blank" href="https://blockchain.info/payment_request?address=18uuUZ5GaMFoRH5TrQFJATQgqrpXCtqZRQ"><img src="https://img.shields.io/badge/Donate-Bitcoin-green.svg"/></a>
-
-[Click here](https://github.com/nitaybz?utf8=%E2%9C%93&tab=repositories&q=homebridge) to review more of my plugins.
-_________________________________________
-
-## Switch Services
-
-### Switch (standard on/off)
-Meant to be used as a standard on/off switch. Light, projector, fan, etc.
-
+Configuration:
 ```
-{
-        "accessory": "HttpMultiswitch",
-        "switch_type": "Switch",
-        "name": "My Projector",
-        "http_method": "GET",
-        "base_url": "http://192.168.0.XXX/command",
-        "on_url": "/on",
-        "off_url": "/off"
-}
+ {
+            "accessory": "SSHomeMode",
+            "name": "Security Cameras",
+            "url": "https://192.168.1.x:5000/",
+            "username": "user",
+            "password": "password
+        }
 ```
-
-### Multiswitch (radio buttons)
-Meant to be used as a switcher, where only one input is ever on.
-Automaticaly turns off the other switches when turning on one ofe them.
-
-Multiswitch appends the string, defined under 'multiurls' below, to complete the path of the Url (Must be use in the same order of the devices in "multiswitch".
-For example, when `PC Mode` is selected, the url generated will be `http://192.168.0.XXX/command?id=42786sdf787`. 
-```
-{
-    "accessory": "HttpMultiswitch",
-    "switch_type": "Multiswitch",
-    "name": "My Multiswitch",
-    "http_method": "GET",
-    "base_url": "http://192.168.0.XXX/command?id=",
-    "multiswitch": [
-                "Apple TV Mode",
-                "PC Mode",
-                "Android Mode",
-                "The Music Mode"
-            ],
-    "multiurls": [
-                "43",
-                "42786sdf787",
-                "l1479461871215",
-                "44"
-            ]
-}
-```
-
-## Configuration Params
-
-|             Parameter            |                       Description                       | Required |
-| -------------------------------- | ------------------------------------------------------- |:--------:|
-| `name`                           | name of the accessory                                   |          |
-| `switch_type`                    | `Switch` or `Multiswitch`                               |     ✓    |
-| `base_url`                       | url endpoint for whatever is receiving these requests   |     ✓    |
-| `http_method`                    | `GET` (default), `POST`,  `PUT`, `DELETE`               |          |
-| `username`                       | username for request                                    |          |
-| `password`                       | password for request                                    |          |
-| `send_immediately`               | option for request                                      |          |
-| `on_url` (only Switch)           | endpoint for the on state                               |     ✓    |
-| `off_url` (only Switch)          | endpoint for the off state                              |     ✓    |
-| `on_body` (only Switch)          | body for on state request                               |          |
-| `off_body` (only Switch)         | body for off state request                              |          |
-| `multiswitch` (only Multiswitch) | list of inputs for the Multiswitch - order is respected |     ✓    |
-| `multiurls` (only Multiswitch)   | list of endpoint urls for multiswitch-order is respected|     ✓    |
-
-## Help
-
-  - Make sure specify a port in the if necessary. (i.e. `"base_url" : "http://192.168.0.XXX:2000"`)
-  - Verify the correct `http_method` is begin used. Switcheroo defaults to `GET`
-
-## Installation
-Read about an example Raspberry Pi + Homebridge setup guide with this package [here](https://github.com/chriszelazo/Apartment-Homebridge-Setup).
-
-1. Install homebridge using: `npm install -g homebridge`
-2. Install homebridge-http using: `npm install -g homebridge-http-multiswitch`
-3. Update your config file
--- index.js
+++ index.js
@@ -7,120 +7,78 @@ var request = require('request');
 module.exports = function (homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
-    homebridge.registerAccessory('homebridge-http-multiswitch', 'HttpMultiswitch', HttpMultiswitch);
+    homebridge.registerAccessory('homebridge-synology-surveillance-homemode', 'SSHomeMode', HttpMultiswitch);
 };
 
 function HttpMultiswitch(log, config) {
     this.log = log;
 
-    this.name            = config.name             || 'MultiSwitch';
-    this.switchType      = config.switch_type;           
-    this.baseUrl         = config.base_url;
-    this.httpMethod      = config.http_method      || 'GET';
+    this.name = config.name || 'MultiSwitch';
+    this.url = config.url;
 
-    this.username        = config.username         || '';
-    this.password        = config.password         || '';
-    this.sendImmediately = config.send_immediately || '';
-
-    switch (this.switchType) {
-        case 'Switch':
-            this.onUrl   = this.baseUrl + config.on_url;
-            this.offUrl  = this.baseUrl + config.off_url;
-            this.onBody  = config.on_body  || '';
-            this.offBody = config.off_body || '';
-            break;
-
-        case 'Multiswitch':
-            this.multiswitch = config.multiswitch;
-            this.multiurls = config.multiurls;
-            break;
-
-        default:
-            throw new Error('Unknown homebridge-http-multiswitch switch type');
-    }
+    this.username = config.username || '';
+    this.password = config.password || '';
+    this.sessionToken = "";
 }
 
 HttpMultiswitch.prototype = {
 
-    httpRequest: function(url, body, method, username, password, sendimmediately, callback) {
+
+    httpRequest: function (path, callback, recursive) {
+        var _this = this;
         request({
-            url: url,
-            body: body,
-            method: method,
-            rejectUnauthorized: false,
-            auth: {
-                user: username,
-                pass: password,
-                sendImmediately: sendimmediately
+                url: this.url + path + "&_sid=" + this.sessionToken,
+                method: "GET",
+                rejectUnauthorized: false,
+            },
+            function (error, response, body) {
+                var resp = JSON.parse(body);
+                if (resp.success || recursive) {
+                    callback(error, response, body);
+                }
+                else {
+                    _this.httpRequest("/webapi/auth.cgi?api=SYNO.API.Auth&method=Login&version=3&account=" + _this.username + "&passwd=" + _this.password + "&session=SurveillanceStation&format=sid",
+                        function (err, resp, bod) {
+                            var r = JSON.parse(bod);
+                            if (r.success) {
+                                //OK logged in
+                                _this.sessionToken = r.data.sid;
+                                _this.log.info("Logged in.");
+                                //Retry the request
+                                _this.httpRequest(path, callback, true);
+                            }
+                            else {
+                                //Didnt work
+                                _this.log.error("Unable to login " + bod);
+                            }
+                        });
+                }
+            });
+    },
+    getState: function (targetService, callback) {
+        this.httpRequest("/webapi/entry.cgi?api=SYNO.SurveillanceStation.HomeMode&version=1&method=GetInfo", function (error, response, responseBody) {
+            if (error) {
+                this.log.error('getPowerState failed: ' + error.message);
+                this.log('response: ' + response + '\nbody: ' + responseBody);
+
+                callback(error);
+            } else {
+                var resp = JSON.parse(responseBody);
+                callback(error, resp.data.on);
             }
-        },
-        function(error, response, body) {
-            callback(error, response, body);
-        });
+        }.bind(this));
     },
 
-    setPowerState: function(targetService, powerState, callback, context) {
-        var funcContext = 'fromSetPowerState';
-        var reqUrl = '', reqBody = '';
-
-        // Callback safety
-        if (context == funcContext) {
-            if (callback) {
-                callback();
-            }
-
-            return;
-        }
-
-        switch(this.switchType) {
-            case 'Switch':
-                if (!this.onUrl || !this.offUrl) {
-                    this.log.warn('Ignoring request; No power state urls defined.');
-                    callback(new Error('No power state urls defined.'));
-                    return;
-                }
-
-                reqUrl  = powerState ? this.onUrl  : this.offUrl;
-                reqBody = powerState ? this.onBody : this.offBody;
-                break;
-
-            case 'Multiswitch':
-                this.services.forEach(function (switchService, idx) {
-                    if (idx === 0) {
-                        // Don't check the informationService which is at idx=0
-                        return;
-                    }
-
-                    if (targetService.subtype === switchService.subtype) {
-                        reqUrl = this.baseUrl + this.multiurls[idx-1];
-                    } else {
-                        switchService.getCharacteristic(Characteristic.On).setValue(false, undefined, funcContext);
-                    }
-                }.bind(this));
-                break;
-
-            default:
-                this.log('Unknown homebridge-http-multiswitch type in setPowerState');
-        }
-
-        this.httpRequest(reqUrl, reqBody, this.httpMethod, this.username, this.password, this.sendImmediately, function(error, response, responseBody) {
+    setPowerState: function (targetService, powerState, callback) {
+        var state = (powerState ? "off" : "on");
+        this.httpRequest("/webapi/entry.cgi?api=SYNO.SurveillanceStation.HomeMode&version=1&method=Switch&" + state + "=true", function (error, response, responseBody) {
             if (error) {
                 this.log.error('setPowerState failed: ' + error.message);
                 this.log('response: ' + response + '\nbody: ' + responseBody);
-            
+
                 callback(error);
             } else {
-                switch (this.switchType) {
-                    case 'Switch':
-                        this.log.info('==> ' + (powerState ? "On" : "Off"));
-                        break;
-                    case 'Multiswitch':
-                        this.log('==> ' + targetService.subtype);
-                        break;
-                    default:
-                        this.log.error('Unknown switchType in request callback');
-                }
-
+                this.log.info('==> ' + (powerState ? "On" : "Off"));
                 callback();
             }
         }.bind(this));
@@ -136,54 +94,20 @@ HttpMultiswitch.prototype = {
 
         var informationService = new Service.AccessoryInformation();
         informationService
-            .setCharacteristic(Characteristic.Manufacturer, 'Http-MultiSwitch')
-            .setCharacteristic(Characteristic.Model, 'Http-MultiSwitch');
+            .setCharacteristic(Characteristic.Manufacturer, 'Synology')
+            .setCharacteristic(Characteristic.Model, 'Surveillance Station');
         this.services.push(informationService);
 
-        switch (this.switchType) {
-            case 'Switch':
-                this.log('(switch)');
 
-                var switchService = new Service.Switch(this.name);
-                switchService
-                    .getCharacteristic(Characteristic.On)
-                    .on('set', this.setPowerState.bind(this, switchService));
+        var switchService = new Service.Switch(this.name);
+        switchService
+            .getCharacteristic(Characteristic.On)
+            .on('set', this.setPowerState.bind(this, switchService))
+            .on('get', this.getState.bind(this, switchService));
 
-                this.services.push(switchService);
+        this.services.push(switchService);
 
-                break;
-            case 'Multiswitch':
-                this.log('(multiswitch)');
-
-                for (var i = 0; i < this.multiswitch.length; i++) {
-                    var switchName = this.multiswitch[i];
-
-                    switch(i) {
-                        case 0:
-                            this.log.warn('---+--- ' + switchName); break;
-                        case this.multiswitch.length-1:
-                            this.log.warn('   +--- ' + switchName); break;
-                        default:
-                            this.log.warn('   |--- ' + switchName);
-                    }
-
-                    var switchService = new Service.Switch(switchName, switchName);
-
-                    // Bind a copy of the setPowerState function that sets 'this' to the accessory and the first parameter
-                    // to the particular service that it is being called for. 
-                    var boundSetPowerState = this.setPowerState.bind(this, switchService);
-                    switchService
-                        .getCharacteristic(Characteristic.On)
-                        .on('set', boundSetPowerState);
-
-                    this.services.push(switchService);
-                }
-
-                break;
-            default:
-                this.log('Unknown homebridge-http-multiswitch type in getServices');
-        }
-        
         return this.services;
     }
 };
+
